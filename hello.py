54f95afe214dee3b8ac039073be651a4fd3a76b8
Rob Nolen <rob.nolen@gmail.com>
2015-09-10 14:16:47 -0700

    added color change and chuck norris quote

--- hello.py
+++ hello.py
@@ -2,6 +2,7 @@ import os
 import uuid
 import redis
 import json
+import requests
 from flask import Flask
 
 
@@ -9,12 +10,12 @@ app = Flask(__name__)
 my_uuid = str(uuid.uuid1())
 BLUE = "#0099FF"
 GREEN = "#33CC33"
-COLOR = GREEN
 
 # default redis config
 redis_host = os.getenv('REDIS_HOST')
 redis_port = os.getenv('REDIS_PORT')
 redis_pw   = os.getenv('REDIS_PW')
+COLOR = GREEN
 
 #if VCAP_SERVICES is not Nil, then we are in CF, and use rediscloud service
 if not os.getenv('VCAP_SERVICES') == None:
@@ -23,9 +24,16 @@ if not os.getenv('VCAP_SERVICES') == None:
     redis_host = service_creds['hostname']
     redis_port = service_creds['port']
     redis_pw   = service_creds['password']
-
+    COLOR=BLUE
 myredis = redis.Redis(host=redis_host, port=redis_port, password=redis_pw)
 
+
+#get quote of the day
+def get_quote():
+    request_api = "http://api.icndb.com/jokes/random"
+    req = requests.get(request_api)
+    quote = json.loads(req.text)['value']['joke']
+    return quote
 
 def getcounter(): 
     counter = myredis.get('pagecount')
@@ -41,6 +49,7 @@ def updatecounter(count):
 def hello():
     count = getcounter()
     updatecounter(count)
+    quote = get_quote()
     return """
     <html>
     <body bgcolor="{}">
@@ -49,13 +58,16 @@ def hello():
     {}</br>
     <br>
     <br>
+    QOTD: {}
+    <br>
+    <br>
     Visits: {} 
 
     </center>
     
     </body>
     </html>
-    """.format(COLOR,my_uuid,count)
+    """.format(COLOR,my_uuid,quote,count)
 
 if __name__ == "__main__":
 	app.run(debug=True,host='0.0.0.0', port=int(os.getenv('VCAP_APP_PORT', '5000')))
