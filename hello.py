b961fbac2179960ac9e84f3985db1ce052cb94d6
Rob Nolen <rob.nolen@gmail.com>
2015-09-09 18:42:35 -0700

    text based counter

--- hello.py
+++ hello.py
@@ -9,24 +9,34 @@ BLUE = "#0099FF"
 GREEN = "#33CC33"
 
 COLOR = GREEN
+def getcounter():
+    with open('counter', 'r+') as f:
+        counter = int(f.readline())
+    return counter
+
+def updatecounter(count):
+    with open('counter', 'w+') as f:
+        f.write(str(count+1))
 
 @app.route('/')
 def hello():
-
-
+    count = getcounter()
+    updatecounter(count)
     return """
     <html>
     <body bgcolor="{}">
 
     <center><h1><font color="white">Hi, I'm GUID:<br/>
     {}</br>
-
+    <br>
+    <br>
+    Visits: {} 
 
     </center>
-
+    
     </body>
     </html>
-    """.format(COLOR,my_uuid,)
+    """.format(COLOR,my_uuid,count)
 
 if __name__ == "__main__":
 	app.run(debug=True,host='0.0.0.0', port=int(os.getenv('VCAP_APP_PORT', '5000')))
